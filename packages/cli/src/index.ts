199666c3b93b2c5bfb32bed48ecaaefcce8cf39d
Nicolas <divicolas@gmail.com>
2026-03-02 13:04:22 +0100

    feat: include command

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -2,12 +2,14 @@
 import { Command } from "@commander-js/extra-typings";
 import { runCommand } from "./commands/run";
 import { validateCommand } from "./commands/validate";
+import { initCommand } from "./commands/init";
 
 const program = new Command()
   .name("colony")
   .description("Deploy and manage autonomous ant colonies")
   .version("0.1.0");
 
+program.addCommand(initCommand);
 program.addCommand(runCommand);
 program.addCommand(validateCommand);
 
